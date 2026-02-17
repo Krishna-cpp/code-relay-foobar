76fa96e594f639d06751a97884cc468232531957
Krishna Kumar <krishnasingh1007.k@gmail.com>
2026-02-17 13:20:44 +0530

    db connection fix

--- task-nexus/server/server.js
+++ task-nexus/server/server.js
@@ -14,7 +14,9 @@ const fluxNexusHandler = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false }
 });
 
 fluxNexusHandler.connect((err) => {
